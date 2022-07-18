bf720e48795a79b2a5bd148aced10e7ecd62bed4
yuye <2428392739@qq.com>
2022-07-18 23:30:55 +0800

    '完成初始化'

---
+++ assets/js/login.js
@@ -0,0 +1,63 @@
+$(function () {
+    $('#link_reg').on('click', function () {
+        $('.login-box').hide()
+        $('.reg-box').show()
+    })
+
+    $('#link_login').on('click', function () {
+        $('.login-box').show()
+        $('.reg-box').hide()
+    })
+
+    var from = layui.from
+    var layer = layui.layer
+
+    from.verify({
+        pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+        repwd: function (value) {
+            // 通过形参拿到的是确认密码框的内容
+            // 还需要密码框中的内容，再进行等于判断
+            var pwd = $('.reg-box [name=password]').val()
+            if (pwd !== value) {
+                return '两次密码不一致'
+            }
+        }
+    })
+
+    // 监听注册表单的提交事件
+    $('#form_reg').on('submit', function (e) {
+        e.preventDefault()
+
+        var data = {
+            username: $('#form_reg [name=username]').val(),
+            password: $('#form_reg [name=password]').val()
+        }
+        $.post('/api/reguser', data, function (res) {
+            if (res.status !== 0) {
+                return layer.msg(res.message)
+            }
+            layer.msg('注册成功')
+
+            // 模拟人的点击行为
+            $('#link_login').click()
+        })
+    })
+
+    // 监听登录表单的提交事件
+    $('#form_login').submit(function (e) {
+        e.preventDefault()
+        $.ajax({
+            url: '/api/login',
+            method: 'POST',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('登陆失败')
+                }
+                return layer.msg('登录成功')
+                localStorage.setItem('token', res.token)
+                location.href = '/index.html'
+            }
+        })
+    })
+})
